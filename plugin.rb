50994cb68e958fdcb85d82e9b08be3d54a1dc34a
Tim Bass <neo@unix.com>
2020-12-07 14:36:43 +0700

    first commit

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# name: discourse-neo-email-digest-monkey-patch
+# about: change default behavior of email digests job scheduler
+# version: 0.0.1
+# date: 30 Nov 2020
+# authors: Neo
+# url: https://github.com/unixneo/
+
+PLUGIN_NAME = "discourse-neo-email-digest-monkey-patch"
+
+after_initialize do
+  load File.expand_path("../app/jobs/scheduled/monkey_patch_enqueue_digest_emails.rb", __FILE__)
+end
